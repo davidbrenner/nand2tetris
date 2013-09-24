00dee888e0a60b3503a953d1f4662a8d690c429d
David Brenner <davidb@csh.rit.edu>
2013-09-23 21:23:21 -0400

    Project 4 completed.
    Finished project 4 part B (Fill.asm). asm code to draw the screen black
    when a key is pressed. Screen and keyboard are DMA. Polling is used to
    check keyboard value.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,53 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+(LOOP)
+    @SCREEN
+    D=A
+    @R0
+    M=D // store screen location counter in R0
+
+    (SCREENLOOP)
+        @KBD
+        D=M // set D=keyboard input
+
+        @WHITE
+        D;JEQ // jmp to white if no key was pressed (D=0)
+
+        (BLACK) // else, color screen black
+        @R0
+        A=M
+        M=-1 // set screen to black
+        @END
+        0;JEQ // goto END
+
+        (WHITE)
+        @R0
+        A=M
+        M=0 // set screen to white
+
+        (END)
+        @R0
+        M=M+1
+        D=M
+        @KBD
+        D=D-A
+        @SCREENLOOP
+        D;JNE
+        @LOOP
+        D;JEQ
+    @R0
+    D=M
+    @KBD
+    D=D-A
+    @SCREENLOOP
+    D;JNE
+    @LOOP
+    D;JEQ
+@LOOP
+0;JEQ // infinite loop
+
+
+
+
+
