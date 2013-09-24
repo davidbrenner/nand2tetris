35ca1c4dd8a324c777cea6bf51827930f3942c19
David Brenner <davidb@csh.rit.edu>
2013-09-23 20:47:39 -0400

    Project 4 part A completed.
    Implemented mult.asm code to multiply 2 numbers using addition.

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+// pseudo code: add R1 to itself R0 times
+// for(i=0; i<R0; i++){
+//    R2=R2+R1;
+// }
+
+@R2
+M=0 // R2=0
+
+@i
+M=0 // i=0
+
+(LOOP)
+    @i
+    D=M // D=i
+    @0
+    D=D-M // D=i-R0
+    @END
+    D;JGE // if i-R0 >= 0 goto END
+
+    @1
+    D=M // D=R1
+    @2
+    M=D+M // R2=R2+R1
+    @i
+    M=M+1 // i=i+1
+    @LOOP
+    0; JMP // repeat
+(END)
+    @END
+    0; JMP // infinite loop
